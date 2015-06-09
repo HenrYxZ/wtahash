b5e845ece73bf898dd5a533680f7369800fec1e7
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-09 16:54:28 -0300

    testeando rankings sin usar numpy

--- evaluation.py
+++ evaluation.py
@@ -68,7 +68,7 @@ class Evaluation:
             rankings = data["stored"]
         else:
             rankings = self.get_rankings(test_data, wta_hash)
-            self.store_rankings(rankings)
+            self.store_rankings(rankings, ranking_size)
             self.store_labels(train_labels, test_labels)
 
         # Dot products
@@ -227,7 +227,7 @@ class Evaluation:
         print("Storing rankings in a mat file ...")
         start = time.time()
         rankings_filename = "results/rankings_{0}.mat".format(self.n_classes)
-        data = {"stored": rankings[:][:ranking_size]}
+        data = {"stored": rankings}
         sio.savemat(rankings_filename, data)
         end = time.time()
         s = "Elapsed time storing the rankings {0} secs.".format(end - start)
