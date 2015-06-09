3f4fa52a81289e11760a38068c8a91d3e9f8067c
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-09 18:16:42 -0300

    error, no estaba usando el largo de train para el ranking size

--- evaluation.py
+++ evaluation.py
@@ -67,7 +67,7 @@ class Evaluation:
             rankings = data["stored"]
         else:
             rankings = self.get_rankings(test_data, wta_hash)
-            ranking_size = min((2500, len(rankings)))
+            ranking_size = min((2500, len(train_data)))
             self.store_rankings(rankings, ranking_size)
             self.store_labels(train_labels, test_labels)
 
