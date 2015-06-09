6b65f75cb02b42e2ea3e7ce4b30e8687c4ca039d
Hernaldo Jesus Henriquez <hjhenriq@gmail.com>
2015-06-09 15:55:44 -0300

    probar el tipo de la matrix de ranking

--- testing_script.py
+++ testing_script.py
@@ -4,22 +4,25 @@ import scipy.io as sio
 ''' 
 Testing precision recall function for ranking retrieval
 '''
+def test_prec_recall():
+	r1 = ["R", "F", "R", "F", "F", "F", "F", "F", "R", "R"]
+	r2 = ["F", "R", "F", "F", "R", "R", "R", "F", "F", "F"]
 
-r1 = ["R", "F", "R", "F", "F", "F", "F", "F", "R", "R"]
-r2 = ["F", "R", "F", "F", "R", "R", "R", "F", "F", "F"]
+	precision, recall = utils.precision_recall(r1)
+	interpolated_p = utils.interpolate_p(precision)
+	print("precision = {0}".format(precision))
+	print("recall = {0}".format(recall))
+	print("interpolated_p = {0}".format(interpolated_p))
 
-# precision, recall = utils.precision_recall(r1)
-# interpolated_p = utils.interpolate_p(precision)
-# print("precision = {0}".format(precision))
-# print("recall = {0}".format(recall))
-# print("interpolated_p = {0}".format(interpolated_p))
-
-# precision, recall = utils.precision_recall(r2)
-# interpolated_p = utils.interpolate_p(precision)
-# print("precision = {0}".format(precision))
-# print("recall = {0}".format(recall))
-# print("interpolated_p = {0}".format(interpolated_p))
+	precision, recall = utils.precision_recall(r2)
+	interpolated_p = utils.interpolate_p(precision)
+	print("precision = {0}".format(precision))
+	print("recall = {0}".format(recall))
+	print("interpolated_p = {0}".format(interpolated_p))
 
 def test_rankings():
 	rankings = sio.loadmat(open("results/rankings_2.mat", "rb"))
 	print("rankings = {0}".format(rankings))
+
+if __name__ == '__main__':
+	test_rankings()
